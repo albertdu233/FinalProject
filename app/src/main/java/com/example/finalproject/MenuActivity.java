a79150cf297d8cba750b329ccb97c35ac5d49b25
alber <albertdu233@gmail.com>
2019-08-13 10:09:58 -0700

    Just add a simple "about us" dialog

--- app/src/main/java/com/example/finalproject/MenuActivity.java
+++ app/src/main/java/com/example/finalproject/MenuActivity.java
@@ -1,9 +1,11 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,7 @@ public class MenuActivity extends AppCompatActivity {
     private DatabaseHelper db;
     private User login;
     private ImageButton menu_btn_pro;
+    private Button menu_aboutus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,8 @@ public class MenuActivity extends AppCompatActivity {
         login = db.getUser(username);
         this.getSupportActionBar().setTitle("Welcome " + username + "!");
         menu_btn_pro = (ImageButton) findViewById(R.id.btn_profile);
+        menu_aboutus = (Button) findViewById(R.id.btn_aboutus);
+
         menu_btn_pro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +40,27 @@ public class MenuActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(),ProfileActivity.class);
                 intent.putExtra("Username", un);
                 startActivity(intent);
+            }
+        });
+
+        menu_aboutus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MenuActivity.this);
+                builder.setTitle("About us!");
+                builder.setMessage("Jingyan Du - 014436615\n" +
+                        "Michell Kuang - 013421094\n" +
+                        "Kishan Sarvaiya - 015658251");
+
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                AlertDialog editEm = builder.create();
+                editEm.show();
             }
         });
 
