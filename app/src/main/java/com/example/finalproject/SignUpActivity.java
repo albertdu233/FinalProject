65d0a0f5950b2c15fe177c95dd9e84b03f190b9a
miche <michellkuang96@yahoo.com>
2019-08-14 19:16:11 -0700

    Added javadoc comments for sign up activity

--- app/src/main/java/com/example/finalproject/SignUpActivity.java
+++ app/src/main/java/com/example/finalproject/SignUpActivity.java
@@ -1,3 +1,6 @@
+/**
+ * This class represents the sign up/registration page.
+ */
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,16 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
     private DatabaseHelper db;
     private Button sp_btn_apply;
     private EditText sp_password;
     private EditText sp_re;
     private EditText sp_email;
     private EditText sp_username;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apply);
         db = new DatabaseHelper(this);
@@ -28,6 +32,10 @@ public class SignUpActivity extends AppCompatActivity {
         sp_re = (EditText)findViewById(R.id.txt_apply_retype);
         sp_btn_apply = (Button)findViewById(R.id.btn_apply);
 
+        /**
+         * Sets a listener for the apply button. Checks if all fields are valid
+         * and then registers the account for the user.
+         */
         sp_btn_apply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,53 +43,54 @@ public class SignUpActivity extends AppCompatActivity {
                 String un = sp_username.getText().toString();
                 String pw = sp_password.getText().toString();
                 String re = sp_re.getText().toString();
-                //check if email is empty
+                //checks if the email field is empty
                 if(em.equals("")){
                     Toast.makeText(getApplicationContext(), "The email field is empty!", Toast.LENGTH_SHORT).show();
                 }
-                //Check if the email format is correct
+                //hecks if the email format is correct
                 else if (!emailCheck(em)) {
                     Toast.makeText(getApplicationContext(), "Email format incorrect!",
                             Toast.LENGTH_SHORT).show();
                     sp_email.setText("");
                 }
-                //check if the email is already used
+                //checks if the email is already used
                 else if(db.checkEmail(em)==false){
                     Toast.makeText(getApplicationContext(), "This email is already registered", Toast.LENGTH_SHORT).show();
                     sp_email.setText("");
                 }
-                //check if the username is empty
+                //checks if the username field is empty
                 else if(un.equals("")){
                     Toast.makeText(getApplicationContext(), "The username field is empty!", Toast.LENGTH_SHORT).show();
                 }
-                //check if the username is too short
+                //checks if the username is too short
                 else if (un.length() < 3) {
                     Toast.makeText(getApplicationContext(), "The username cannot be less than 3 letters", Toast.LENGTH_SHORT).show();
                     sp_username.setText("");
                 }
-                //check if the username is too long
+                //checks if the username is too long
                 else if (un.length() > 13) {
                     Toast.makeText(getApplicationContext(), "The username cannot be longer than 13 characters", Toast.LENGTH_SHORT).show();
                     sp_username.setText("");
                 }
-                //check if the email is already used
+                //checks if the email is already used
                 else if(db.checkUser(un)==false){
                     Toast.makeText(getApplicationContext(), "This username is already registered", Toast.LENGTH_SHORT).show();
                     sp_username.setText("");
                 }
+                //checks if the password field is empty
                 else if(pw.equals("")){
                     Toast.makeText(getApplicationContext(), "The password field is empty!", Toast.LENGTH_SHORT).show();
                 }
-                //check if the length of password is correct
+                //checks if the length of the password is correct
                 else if (pw.length() < 6) {
                     Toast.makeText(getApplicationContext(), "The password must be at least 6 letters long", Toast.LENGTH_SHORT).show();
                     sp_password.setText("");
                 }
-                //check if the retype password is empty
+                //checks if the retype password is empty
                 else if(re.equals("")){
                     Toast.makeText(getApplicationContext(), "The retype password field is empty!", Toast.LENGTH_SHORT).show();
                 }
-                //check if the retype password matched the password
+                //checks if the retype password matches the password
                 else if(!re.equals(pw)){
                     Toast.makeText(getApplicationContext(), "The passwords do not match!", Toast.LENGTH_SHORT).show();
                 }
@@ -98,7 +107,12 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
-    //Function that use the patterns that android studio has to check the email format.
+
+    /**
+     * Checks to see if the string is in the proper email format.
+     * @param email String to be checked against email format
+     * @return Properly formatted email string
+     */
     boolean emailCheck(String email) {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
